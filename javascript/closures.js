04ac1303f2f147a4c570886408e2532fca01bfa3
Jason Rasmussen <jrasm91@gmail.com>
2017-02-23 11:25:53 -0700

    Update closures.js

--- javascript/closures.js
+++ javascript/closures.js
@@ -1,3 +1,10 @@
+
+/* Closures are when you have a function in a function.
+ *  - function stack frame is "kept in memory"
+ *  - functions variables are linked to closure
+ *  - functions created in the same closure share local variables
+ */
+
 function counter(){
   var count = 0;
   var increment = function(){
@@ -14,12 +21,29 @@ counter1(); // 1
 counter1(); // 2
 counter1(); // 3
 
-// each instance of counter gets it's own copy of local variables
 var counter2 = counter();
 counter2(); // 1
 counter1(); // 4
 counter2(); // 2
 
+function listCounter(list){
+  var result = [];
+  for(i = 0; i < list.length; i++){
+    var name = 'item' + i;
+    var value = list[i];
+    result.push(function(){
+       console.log(name, value);
+    });
+  }
+  return result;
+}
 
+var functions = listCounter([1, 2, 3]);
+for(var i = 0; i < functions.length; i++){
+  functions[i]();
+}
+// item2 undefined
+// item2 undefined
+// item2 undefined
 
 
